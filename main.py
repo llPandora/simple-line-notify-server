d11a580270630d73b2f2669c3c29a63e4f12eb00
llPandora <llharpy@hotmail.com>
2018-02-08 12:09:06 +0900

    finish v2

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 import requests
 import socket
 from flask import Flask, render_template, redirect,request
-app = Flask(__name__)
+application = Flask(__name__)
 
 line_token = 'UawjDRkARXD9k49XaSlEIe3iOwpieifdApbjo3wtrI1'
 
-@app.route('/')
+@application.route('/')
 def root_html():
     return redirect('index')
 
-@app.route('/index')
+@application.route('/index')
 def index_html(name=None):
     return render_template('index.html')
 
 
-@app.route('/tell/',methods=['POST','GET'])
-@app.route('/tell',methods=['POST','GET'])
+@application.route('/tell/',methods=['POST','GET'])
+@application.route('/tell',methods=['POST','GET'])
 def tell_html():
     if request.method == 'GET':
         return render_template('tell.html',result='begin')
@@ -32,5 +32,7 @@ def tell_html():
             return render_template('tell.html',result='Fail to deliver message :\n' + message + '\n')
         return render_template('tell.html',result='Message :\n' + message + '\nDelivered\n')
 
+if __name__ == '__main__':
+    application.run()
 
 #curl -X POST -H 'Authorization: Bearer UawjDRkARXD9k49XaSlEIe3iOwpieifdApbjo3wtrI1' -F 'message=foobar' https://notify-api.line.me/api/notify
